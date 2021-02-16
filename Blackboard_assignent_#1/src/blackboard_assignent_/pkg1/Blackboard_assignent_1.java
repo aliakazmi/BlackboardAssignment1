a0571bee04c060117443dc022105b10994a47b4c
aliakazmi <aliakazmi@localhost>
2021-02-15 19:30:52 -0500

    BlackBoard Assignment #1

---
+++ Blackboard_assignent_#1/src/blackboard_assignent_/pkg1/Blackboard_assignent_1.java
@@ -0,0 +1,18 @@
+
+package blackboard_assignent_.pkg1;
+import java.util.Scanner;
+
+public class Blackboard_assignent_1 {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+         for(int i = 0; i < 100; i += 2 ){
+    System.out.println(i);
+    
+    }
+    
+    }
+}
